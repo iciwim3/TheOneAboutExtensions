f10b5f47155c731f1c8cdf6002ac9a184e096c78
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-12-08 20:58:05 -0800

    1. Added buttons, made IBOutlet and IBAction connections, and created a new Swift file for the extension of the UIButtons.

---
+++ She-Got-More-Extensions-Than-ATT/UIButtonExt.swift
@@ -0,0 +1,21 @@
+//
+//  UIButtonExt.swift
+//  She-Got-More-Extensions-Than-ATT
+//
+//  Created by Sain-R Edwards Jr. on 12/8/17.
+//  Copyright © 2017 Appybuildmore Apps. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func wiggle() {
+        let wiggleAnim = CABasicAnimation(keyPath: "position")
+        wiggleAnim.duration = 0.05
+        wiggleAnim.repeatCount = 5
+        wiggleAnim.autoreverses = true
+        wiggleAnim.fromValue = CGPoint(x: self.center.x - 4.0, y: self.center.y)
+        wiggleAnim.toValue = CGPoint(x: self.center.x + 4.0, y: self.center.y)
+        layer.add(wiggleAnim, forKey: "position")
+    }
+}
--- She-Got-More-Extensions-Than-ATT/ViewController.swift
+++ She-Got-More-Extensions-Than-ATT/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorizeButton: UIButton!
+    @IBOutlet weak var wiggleButton: UIButton!
+    @IBOutlet weak var dimButton: UIButton!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func colorizeButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func wiggleButtonTapped(_ sender: Any) {
+    }
+    @IBAction func dimButtonTapped(_ sender: Any) {
+    }
+    
 }
 
